cfdeabd3533d447ff1702bde2d9e1bbac8ac2235
hjewnd2k <73420420+hjewnd2k@users.noreply.github.com>
2021-07-08 14:17:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Huyền à!',
+    introTitle: 'Tên người yêu của bạn à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
