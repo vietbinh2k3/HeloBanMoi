f7cf279ea8f58f9efb109e0a07ce476e59bf3a7b
BEARmei <87651857+BEARmei@users.noreply.github.com>
2021-07-19 23:57:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ const CONFIG = {
     Đằng sau chữ yêu đây là thương
     Tớ có điều muốn hỏi cậu đc ko?`,
     btnIntro: 'oki😁',
-    title: 'Cậu thích tớ phải ko😗',
+    title: 'Cậu thích tớ phải ko? (´。＿。｀)',
     desc: 'Nếu cậu ko trả lời mà thoát ra là muốn làm ny tớ rùi đó nha:3',
     btnYes: 'Yêu ơi là yêu <33',
     btnNo: 'Cậu bị ảo à:3',
     question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ zậy (ㅅ˙³˙)♡',
     btnReply: 'Gửi cho cậu <3',
     reply: 'Nhìn cậu dễ thương vl =))',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Tớ biết mà (♥ω♥*). Yêu cậu nhiều lắm (っ'-')╮=͟͟͞͞💌',
     messDesc: 'Tối nay 7h tớ ra đón đi chơi nhé công chúa.Còn chờ gì nữa mà ko inbox cho tớ luôn đi ❤',
     btnAccept: 'Okii <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
