c24f192ab933dac3d3df04aa5c84c7caa5dea379
vietbinh2k3 <93721021+vietbinh2k3@users.noreply.github.com>
2021-11-05 12:50:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,11 +9,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Em thích anh <33',
     btnNo: 'Không, Em coi anh là bạn thui :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao 8 lại yêu 5 <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho Em iu của anh nè  <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu 8 à <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu anh à <33333333',
     mess: 'Anh biết mà 🥰. Yêu Em nhiều nhiều 😘😘',
     messDesc: 'Hết dịch anh dẫn em đi chơi nha <3 iu em lắm ó:>>.',
     btnAccept: 'dạ anh iu <3',
-    messLink: 'https://www.facebook.com/binh.trinhviet.7543/'
+    messLink: 'https://www.messenger.com/t/100025317167936'
 }
